ad458cc7c874288f403e4f29a28a8828fbc27216
Camila Wolter <camila0wolter@gmail.com>
2023-01-27 18:24:17 -0300

    refactor: alterando as querys e filtros

--- app/backend/src/controllers/leaderboardController.ts
+++ app/backend/src/controllers/leaderboardController.ts
@@ -2,15 +2,15 @@ import { Request, Response } from 'express';
 import leaderboardService from '../services/leaderboardService';
 
 const getLeaderboardHome = async (req: Request, res: Response): Promise<Response> => {
-  const [result] = await leaderboardService.getLeaderboardHome();
+  const result = await leaderboardService.getLeaderboardHome();
 
-  return res.status(200).json(result);
+  return res.status(200).json(result.message);
 };
 
 const getLeaderboardAway = async (req: Request, res: Response): Promise<Response> => {
-  const [result] = await leaderboardService.getLeaderboardAway();
+  const result = await leaderboardService.getLeaderboardAway();
 
-  return res.status(200).json(result);
+  return res.status(200).json(result.message);
 };
 
 const getLeaderboardHomeAway = async (req: Request, res: Response): Promise<Response> => {
